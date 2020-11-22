12967e3fc05c3df1120c4d28863e4ea7bde541a9
vagrant <vagrant@localhost.localdomain>
2020-11-22 12:43:45 +0000

    [Finish]ログイン画面の作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,28 @@
 Rails.application.routes.draw do
   root to: 'homes#top'
   get 'homes/about' => 'homes#about'
-  devise_for :admins
-  devise_for :customers
+  # devise_for :admins
+  # devise_for :customers
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  #  devise_for :admins, :controllers => {
+  #   :registrations => 'admins/registrations',
+  #   :sessions => 'admins/sessions'
+  # }
+
+  # devise_scope :admin do
+  #   get "sign_in", :to => "admins/sessions#new"
+  #   get "sign_out", :to => "admins/sessions#destroy"
+  # end
+
+  devise_for :admins, controllers: {
+  sessions:      'admins/sessions',
+  passwords:     'admins/passwords',
+  registrations: 'admins/registrations'
+}
+devise_for :customers, controllers: {
+  sessions:      'customers/sessions',
+  passwords:     'customerss/passwords',
+  registrations: 'customers/registrations'
+}
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,23 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_21_103256) do
+ActiveRecord::Schema.define(version: 2020_11_21_114659) do
 
   create_table "addresses", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "admins", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_admins_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_admins_on_reset_password_token", unique: true
   end
 
   create_table "cart_items", force: :cascade do |t|
